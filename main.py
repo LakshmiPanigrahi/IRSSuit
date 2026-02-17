e0591cbbe6e67866a173850307888db5d7b45da8
LakshmiPanigrahi <swathi.sri.asn@gmail.com>
2026-02-17 10:35:27 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -124,16 +124,13 @@ async def control_library_data(search: str = Query(None)):
     cursor = conn.cursor(dictionary=True)
     try:
         query = """
-            SELECT c.control_id, c.control_name AS title, p.process_name, p.process_owner, p.frequency,
-                   r.likelihood AS risk_level, r.status
-            FROM control c
-            LEFT JOIN risk_control_map rcm ON c.control_id = rcm.control_id
-            LEFT JOIN risk r ON rcm.risk_id = r.risk_id
-            LEFT JOIN process_subprocess_risk_map psrm ON r.risk_id = psrm.risk_id AND psrm.pro_subpro_type = 'PROCESS'
-            LEFT JOIN process_subprocess_map psm ON psrm.pro_subpro_id = psm.process_id
-            LEFT JOIN processes p ON psm.process_id = p.process_id
-            WHERE (%s IS NULL OR %s = '' OR c.control_name LIKE %s)
-        """
+                   SELECT c.control_id, c.control_name AS title, p.process_name, p.process_owner, p.frequency, r.likelihood AS risk_level, r.status FROM control c
+                   LEFT JOIN risk_control_map rcm ON c.control_id = rcm.control_id
+                   LEFT JOIN risk r ON rcm.risk_id = r.risk_id
+                   LEFT JOIN process_subprocess_risk_map psrm ON r.risk_id = psrm.risk_id AND psrm.pro_subpro_type = 'PROCESS'
+                   LEFT JOIN processes p ON psrm.pro_subpro_id = p.process_id
+                   WHERE (%s IS NULL OR %s = '' OR c.control_name LIKE %s);
+                """
         cursor.execute(
             query,
             (search, search, f"%{search}%")
@@ -156,6 +153,29 @@ async def ai_response(value: str = Body(...)):
         """
         cursor.execute(query)
         return cursor.fetchone()
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.get("/control_testing")
+async def control_library_data(search: str = Query(None)):
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+    try:
+        query = """
+                   SELECT c.control_id, c.control_name AS title,c.description as review, p.process_name, p.process_owner, p.frequency, r.likelihood AS risk_level, r.status FROM control c
+                   LEFT JOIN risk_control_map rcm ON c.control_id = rcm.control_id
+                   LEFT JOIN risk r ON rcm.risk_id = r.risk_id
+                   LEFT JOIN process_subprocess_risk_map psrm ON r.risk_id = psrm.risk_id AND psrm.pro_subpro_type = 'PROCESS'
+                   LEFT JOIN processes p ON psrm.pro_subpro_id = p.process_id
+                   WHERE (%s IS NULL OR %s = '' OR c.control_name LIKE %s) ORDER BY c.control_id DESC  LIMIT 2;
+                """
+        cursor.execute(
+            query,
+            (search, search, f"%{search}%")
+        )
+        return cursor.fetchall()
     finally:
         cursor.close()
         conn.close()
